883cd49933666137122d9e877f007c5b7d61a09e
genobili@gmail.com <maoyifeng@xunlei.com>
2017-08-03 16:45:49 +0800

    n

--- comiservice/http.go
+++ comiservice/http.go
@@ -2,10 +2,7 @@ package main
 
 import (
 	"comiservice/http_api"
-	"fmt"
 	"net/http"
-
-	"time"
 
 	"github.com/julienschmidt/httprouter"
 )
@@ -35,27 +32,4 @@ func newHttpServer(ctx *Context) *httpServer {
 
 func (s *httpServer) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	s.router.ServeHTTP(w, req)
-}
-
-func (s *httpServer) epinfo(w http.ResponseWriter, req *http.Request, ps httprouter.Params) (interface{}, error) {
-	// reqParams, err := http_api.NewReqParams(req)
-	// if err != nil {
-	// 	return nil, http_api.Err{400, "INVALID_REQUEST"}
-	// }
-	// log.Println(reqParams)
-	time.Sleep(5e9)
-	fmt.Fprintf(w, "epinfo ret = 11111")
-	channels := "xxxxxxxx"
-	return map[string]interface{}{
-		"channels": channels,
-	}, nil
-}
-
-func (s *httpServer) getpost(w http.ResponseWriter, req *http.Request, ps httprouter.Params) (interface{}, error) {
-	channels := "yyyyyyyy"
-	s.ctx.comiservice.logf(LOG_INFO, "channels = %s", channels, nil)
-	fmt.Fprintf(w, "getpost ret = 0")
-	return map[string]interface{}{
-		"channels": channels,
-	}, nil
 }
