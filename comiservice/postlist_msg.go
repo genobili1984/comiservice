0920eafac96157ad82c6d69619e17ea226131d9a
genobili@gmail.com <maoyifeng@xunlei.com>
2017-08-04 17:45:55 +0800

    111

--- comiservice/postlist_msg.go
+++ comiservice/postlist_msg.go
@@ -4,12 +4,23 @@ import (
 	"fmt"
 	"net/http"
 
+	"comiservice/db"
+
 	"github.com/julienschmidt/httprouter"
 )
 
 func (s *httpServer) getpost(w http.ResponseWriter, req *http.Request, ps httprouter.Params) (interface{}, error) {
+	dbMaster := dbmanager.GetDB(dbmanager.DBMaster)
+	if dbMaster != nil {
+		_, err := dbMaster.Exec("update comico_online.t_comico_info set comico_auth = '五仁月饼' where comico_id = 77;")
+		if err != nil {
+			panic(err.Error())
+		}
+	} else {
+		s.ctx.comiservice.logf(LOG_INFO, "dbMaster is nil ", nil)
+	}
+
 	channels := "yyyyyyyy"
-	s.ctx.comiservice.logf(LOG_INFO, "channels = %s", channels, nil)
 	fmt.Fprintf(w, "getpost ret = 0")
 	return map[string]interface{}{
 		"channels": channels,
